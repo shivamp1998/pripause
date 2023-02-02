8d6d66de6f5dbd0148937d751398ac1e6c06c147
shivamp1998 <shivampandey98@gmail.com>
2023-02-02 15:22:34 +0530

    [fix]

--- content.js
+++ content.js
@@ -1,33 +1,26 @@
-
-
 const multi_pause = () => {
-     const audioNL = document.getElementsByTagName('audio');
-     const videoNL = document.getElementsByTagName('video');
+  const audioNL = document.getElementsByTagName("audio");
+  const videoNL = document.getElementsByTagName("video");
 
-     const audioA = Array.prototype.slice.call(audioNL);
-     const videoA = Array.prototype.slice.call(videoNL);
-     audioA && audioA.forEach((value) => {
-        value.pause();
-     });
-     videoA && videoA.forEach((value) => {
-        value.pause();
-     }) 
-     
-      //spotify
-      const ele = document.querySelector('button[data-testid="control-button-playpause"]');
-      if(ele) ele.click()
-}
-
-
-navigator.mediaDevices.enumerateDevices()
-    .then((devices) => {
-      devices.forEach((device) => {
-        console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
-      });
-    })
-    .catch((err) => {
-      console.error(`${err.name}: ${err.message}`);
+  const audioA = Array.prototype.slice.call(audioNL);
+  const videoA = Array.prototype.slice.call(videoNL);
+  audioA &&
+    audioA.forEach((value) => {
+      value.pause();
+    });
+  videoA &&
+    videoA.forEach((value) => {
+      value.pause();
     });
 
+  //spotify
+  const ele = document.querySelector('button[data-testid="control-button-playpause"]');
+  if (ele) ele.click();
+};
 
-navigator.mediaDevices.ondevicechange = () => multi_pause(); 
+
+
+navigator.mediaDevices.ondevicechange = (event) => {
+  multi_pause();
+}
+
